982a63533d3fd760a9e0de8b485d47802da37392
mangolorex <mangolorex@gmail.com>
2018-04-24 06:07:46 +0000

    Add files via upload

---
+++ TinyHouseTest.ino
@@ -0,0 +1,72 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This sketch shows how to read values from Virtual Pins
+
+  App project setup:
+    Slider widget (0...100) on Virtual Pin V1
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "dfc0a0aa6824483bb7e1ce96b43ab765";
+
+// This function will be called every time Slider Widget
+// in Blynk app writes values to the Virtual Pin V1
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+  // process received value
+}
+void RED(){
+  digitalWrite(14, HIGH);
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+}
+
+void setup()
+{
+  pinMode(14, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth);
+
+}
+
+void loop()
+{
+  Blynk.run();
+  if(pinValue == 1){
+    RED();
+  }
+}
